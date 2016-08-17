8899c3fcc561b081a5ed8825eace8b85fa839ab8
Wade Wegner <wade.wegner@gmail.com>
2016-08-17 15:15:19 +0200

    Cleaned up the tabs

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,10 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, TabBarIOS } from 'react-native';
+import { AppRegistry, TabBarIOS } from 'react-native';
 import AppNavigator from './app/navigation/AppNavigator'
-import Icon from 'react-native-vector-icons/Octicons'
-import Icon2 from 'react-native-vector-icons/EvilIcons'
+import Octicons from 'react-native-vector-icons/Octicons'
+import EvilIcons from 'react-native-vector-icons/EvilIcons'
 
 class abcrn extends Component {
   constructor(props) {
@@ -18,39 +18,35 @@ class abcrn extends Component {
   render() {
     return (
 
-      <TabBarIOS
-        selectedTab={this.state.selectedTab}>
-      
-      <Icon.TabBarItemIOS
-        selected={this.state.selectedTab === "peopleTab"}
-        title={`PEOPLE`}
-        onPress={() => this.setState({selectedTab: "peopleTab"})}
-        iconName="organization">
+      <TabBarIOS selectedTab={this.state.selectedTab}>
+        
+        <Octicons.TabBarItemIOS
+          selected={this.state.selectedTab === "peopleTab"}
+          title={`PEOPLE`}
+          onPress={() => this.setState({selectedTab: "peopleTab"})}
+          iconName="organization">
 
-          <AppNavigator 
-            initialRoute={{ident: "PeopleIndex"}}
-          />
+            <AppNavigator 
+              initialRoute={{ident: "PeopleIndex"}}
+            />
 
-      </Icon.TabBarItemIOS>
+        </Octicons.TabBarItemIOS>
 
-      <Icon2.TabBarItemIOS
-        selected={this.state.selectedTab === "settingsTab"}
-        title={`SETTINGS`}
-        onPress={() => this.setState({selectedTab: "settingsTab"})}
-        iconName="gear">
+        <EvilIcons.TabBarItemIOS
+          selected={this.state.selectedTab === "settingsTab"}
+          title={`SETTINGS`}
+          onPress={() => this.setState({selectedTab: "settingsTab"})}
+          iconName="gear">
 
-          <AppNavigator 
-            initialRoute={{ident: "Settings"}}
-          />
+            <AppNavigator 
+              initialRoute={{ident: "Settings"}}
+            />
 
-      </Icon2.TabBarItemIOS>
+        </EvilIcons.TabBarItemIOS>
 
       </TabBarIOS>
     );
   }
 }
-
-const styles = StyleSheet.create({
-});
 
 AppRegistry.registerComponent('abcrn', () => abcrn);
